2a4dab67a53e9b79a13fe0d1310c7201408dfacc
Manu-3100 <fernandezdebasamanuel@gmail.com>
2024-12-11 14:48:57 +0100

    11/12/2024

--- PruebaJDBC/src/main/java/main/Main.java
+++ PruebaJDBC/src/main/java/main/Main.java
@@ -17,19 +17,24 @@ public class Main {
 		try (Connection con = DriverManager.getConnection(url, usuario, password); 
 			 Statement stmt = con.createStatement();) {
 			
-			String companyName = "Alfreds Futterkiste";
+			String companyName = "' or 1=1; --";
 			String contactName = "Maria Anders";
 			
-			ResultSet rs = stmt.executeQuery("Select count(*) " +
-											 "from customers " +
-											 "where CompanyName = '" + companyName + "' and " + 
-											 "ContactName = '" + contactName + "'" );						
+			try (ResultSet rs = stmt.executeQuery(
+				"Select count(*) " +
+				"from customers " +
+				"where CompanyName = '" + companyName + "' and " + 
+				"ContactName = '" + contactName + "'" ); ){						
+			
 			rs.next();
-			if (rs.getInt(1) != 1) {
+			if (rs.getInt(1) != 0) {
 				System.out.println("Error de validación");
 			}
 			else
 				System.out.println("Validación correcta");
+			} catch (SQLException e) {
+				System.err.println(e.getMessage());
+			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
